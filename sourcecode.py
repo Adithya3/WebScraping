a8a16deb75fe9159686f703639d1424fb20a348b
Adithya3 <bv.adithya369@gmail.com>
2016-09-02 18:09:43 -0700

    sourcecode.py completed

--- sourcecode.py
+++ sourcecode.py
@@ -1,1 +1,18 @@
-import mechanize
+#can get the sourcecode of the website
+
+from bs4 import BeautifulSoup
+import requests
+import os
+
+
+#taking the url and the location to where text file should be created
+url=raw_input('enter the url') #enter the url 
+
+#using requests and beautifulsoup to extract content
+r=requests.get(url)
+soup=BeautifulSoup(r.content,"lxml")
+file_object=open("imageurl.txt","w")
+#writing the webcontent into a file
+web_content=str(soup)
+file_object.write(web_content)
+
