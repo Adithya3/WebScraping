a1aacdf74d862171863cf56ca8174e592d72427c
Adithya3 <bv.adithya369@gmail.com>
2017-02-14 00:18:10 -0800

    csv format to json conversion

---
+++ csv_json.py
@@ -0,0 +1,23 @@
+import os
+import pprint
+import csv
+
+
+def csv_json(DATAFILE_LOCATION1,DATAFILE_LOCATION2):
+	data=[]
+	n=0
+	with open(DATAFILE_LOCATION,'rb') as sd:
+		r=csv.DictReader(sd)
+		for i in r:
+			data.append(i)
+	with open(DATAFILE_LOCATION2,"WB") as bb:
+		bb.write(data)
+
+
+
+
+if __name__=="__main__":
+	DATAFILE_LOCATION1=raw_input("Enter the csv file location")
+	#pprint.pprint(data)
+	DATAFILE_LOCATION2=raw_input("enter the file where json will be stored")
+
